faa7fe432e442d399c92f293aec3856367121e98
maurope <mauricio.penuela@hotmail.com>
2025-07-14 10:54:12 -0500

    Code update

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -22,6 +22,11 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Profile(models.Model):
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+    website = models.URLField() # Para que el autor publique su página web
+    biography = models.TextField(max_length=500)
 
 class Book(models.Model):
     title = models.TextField(max_length=200)
