e5af19835bbd135d4f52a994f508ce730a3ebea7
maurope <mauricio.penuela@hotmail.com>
2025-07-16 06:13:22 -0500

    Code update

--- my_first_project/my_first_app/views.py
+++ my_first_project/my_first_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+from django.views.generic import TemplateView
 
 # Create your views here.
 def my_view(request):
@@ -8,3 +10,18 @@ def my_view(request):
     ]
     context = {'car_list': car_list}
     return render(request, 'my_first_app/car_list.html', context)
+
+class CarlistView(TemplateView):
+    template_name = 'my_first_app/car_list.html'
+
+    def get_context_data(self):
+        car_list = [
+            {'title': 'BMW'},
+            {'title': 'Mazda'},
+        ]
+        return {'car_list': car_list}
+
+def my_test_view(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse('')
